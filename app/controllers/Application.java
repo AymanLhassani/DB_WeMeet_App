e8594af349c34457ceefa01c4d8d24f0b3affb61
AymanLhassani <ayman.lhassani@estudiantat.upc.edu>
2024-11-18 16:42:10 +0100

    3r commit, con la pagina donde muestra una tabla con el numero de espacioes disponibles para alquilar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import play.mvc.*;
 
 import models.*;
+import java.util.List;
 
 public class Application extends Controller
 {
@@ -31,7 +32,7 @@ public class Application extends Controller
     {
         USER ob1 = new USER("ayman", "1234", 3, 1);
         ob1.save();
-        SPACEMEETING ob3 = new SPACEMEETING(4, "Monday-9h::21h","Castelldefels",  true);
+        SPACEMEETING ob3 = new SPACEMEETING(4, "Monday-9h:21h","Castelldefels",  true);
         ob3.save();
         MEETING ob2 = new MEETING(  3, "4/10/24-12h00");
         ob2.save();
@@ -84,6 +85,12 @@ public class Application extends Controller
     public static  void InitReg()
     {
         RegisterHTML();
+    }
+
+    public static void listSpaces(){
+        List<SPACEMEETING> spaceses = SPACEMEETING.findAll();
+        render("Application/listSpace.html", spaceses);
+
     }
 
 }
